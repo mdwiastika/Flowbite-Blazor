3a4d76c304d87f89e0fe2267c7f3497c02dd300d
unknown <marceldwias@gmail.com>
2026-01-10 11:37:09 +0700

    chore: setup flowbite for blazor development

---
+++ Pages/FlowbitePage.cs
@@ -0,0 +1,19 @@
+﻿using BlazorApp2.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp2.Pages;
+
+public abstract class FlowbitePage : ComponentBase
+{
+    [Inject]
+    protected IFlowbiteService FlowbiteService { get; set; } = default!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await FlowbiteService.InitializeFlowbiteAsync();
+        }
+        await base.OnAfterRenderAsync(firstRender);
+    }
+}
