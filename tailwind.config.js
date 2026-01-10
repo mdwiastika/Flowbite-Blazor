d0aeb43247a23d8fe596788bf1e3d0bbd02b3b3f
unknown <marceldwias@gmail.com>
2026-01-10 14:10:44 +0700

    feature: sidebar compability

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./**/*.razor",
+        "./**/*.html",
+        "./wwwroot/**/*.js"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Outfit', 'ui-sans-serif', 'system-ui'],
+            },
+        },
+    },
+    plugins: [],
+}
